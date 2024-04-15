3fab4ba81361e4f7e28072cc1549ed0e209bca32
Rafael de Luca <deluca1712@gmail.com>
2024-04-15 11:29:08 -0300

    menu switch

--- src/atividade04/interfaces/Teste.java
+++ src/atividade04/interfaces/Teste.java
@@ -9,19 +9,51 @@ public class Teste {
 		Scanner input = new Scanner(System.in);
 
 		int opcao = 0;
-		do {			
-			menuInicial();
-			opcao = input.nextInt();
-			
+
+		do {
+			try {
+				menuInicial();
+				opcao = Integer.parseInt(input.nextLine());
+				switch (opcao) {
+				case 1:
+					cadastrarVeiculoPasseio();
+					break;
+				case 2:
+					cadastrarVeiculoCarga();
+					break;
+				case 3:
+					imprimirVeiculosPasseio();
+					break;
+				case 4:
+					imprimirVeiculosCarga();
+					break;
+
+				case 5:
+					imprimirVeiculoPasseioPorPlaca();
+					break;
+				case 6:
+					imprimirVeiculoCargaPorPlaca();
+					break;
+				case 7: 
+					System.out.println("\nSISTEMA ENCERRADO");
+					break;
+				default:
+					System.out.println("\nVocê informou um opção diferente de 1 e 7");
+				}
+
+			} catch (NumberFormatException exception) {
+				System.out.println("Erro. Informar um número inteiro entre 1  e 7");
+				exception.printStackTrace();
+			}
+
 		} while (opcao != 7);
-	
 
 		input.close();
 
 	}
 
 	public static void menuInicial() {
-		
+
 		System.out.print("Sistema de Gestão de Veículos - Menu Inicial" + "\n1. Cadastrar Veiculo de Passeio"
 				+ "\n2. Cadastrar Veiculo de Carga" + "\n3. Imprimir Todos os Veiculos de Passeio"
 				+ "\n4. Imprimir Todos os Veiculos de Carga" + "\n5. Imprimir Veiculo de Passeio pela Placa"
@@ -30,5 +62,28 @@ public class Teste {
 
 	}
 
+	public static void cadastrarVeiculoPasseio() {
+		System.out.println("\nCadastrando um veículo passeio");
+	}
+
+	public static void cadastrarVeiculoCarga() {
+		System.out.println("\nCadastrando um Veiculo de Carga");
+	}
+
+	public static void imprimirVeiculosPasseio() {
+		System.out.println("\nImprimindo todos os veículos de Passeio");
+	}
+
+	public static void imprimirVeiculosCarga() {
+		System.out.println("\nImprimindo todos os veículos de Carga");
+	}
+
+	public static void imprimirVeiculoPasseioPorPlaca() {
+		System.out.println("\nImprimindo veículo de passeio por placa");
+	}
+
+	public static void imprimirVeiculoCargaPorPlaca() {
+		System.out.println("\nImprimindo veículo de carga por placa");
+	}
 
 }
