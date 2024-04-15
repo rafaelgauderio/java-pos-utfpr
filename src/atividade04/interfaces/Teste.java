e5bdfcb780d57954383b0076718c7ee685ce9288
Rafael de Luca <deluca1712@gmail.com>
2024-04-15 15:06:47 -0300

    methods for register and print passanger vehicles

--- src/atividade04/interfaces/Teste.java
+++ src/atividade04/interfaces/Teste.java
@@ -2,27 +2,31 @@ package atividade04.interfaces;
 
 import java.util.Scanner;
 
+import atividade03.heranca.Passeio;
+
 public class Teste {
 
 	public static void main(String[] args) {
 
-		Scanner input = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 
 		int opcao = 0;
+
+		Passeio[] vetorPasseio = new Passeio[5];
 
 		do {
 			try {
 				menuInicial();
-				opcao = Integer.parseInt(input.nextLine());
+				opcao = Integer.parseInt(sc.nextLine());
 				switch (opcao) {
 				case 1:
-					cadastrarVeiculoPasseio();
+					cadastrarVeiculoPasseio(vetorPasseio);
 					break;
 				case 2:
 					cadastrarVeiculoCarga();
 					break;
 				case 3:
-					imprimirVeiculosPasseio();
+					imprimirVeiculosPasseio(vetorPasseio);
 					break;
 				case 4:
 					imprimirVeiculosCarga();
@@ -34,7 +38,7 @@ public class Teste {
 				case 6:
 					imprimirVeiculoCargaPorPlaca();
 					break;
-				case 7: 
+				case 7:
 					System.out.println("\nSISTEMA ENCERRADO");
 					break;
 				default:
@@ -48,13 +52,13 @@ public class Teste {
 
 		} while (opcao != 7);
 
-		input.close();
+		sc.close();
 
 	}
 
 	public static void menuInicial() {
 
-		System.out.print("Sistema de Gestão de Veículos - Menu Inicial" + "\n1. Cadastrar Veiculo de Passeio"
+		System.out.print("\nSistema de Gestão de Veículos - Menu Inicial" + "\n1. Cadastrar Veiculo de Passeio"
 				+ "\n2. Cadastrar Veiculo de Carga" + "\n3. Imprimir Todos os Veiculos de Passeio"
 				+ "\n4. Imprimir Todos os Veiculos de Carga" + "\n5. Imprimir Veiculo de Passeio pela Placa"
 				+ "\n6. Imprimir Veiculo de Carga pela Placa" + "\n7. Sair do Sistema\n\n"
@@ -62,16 +66,83 @@ public class Teste {
 
 	}
 
-	public static void cadastrarVeiculoPasseio() {
-		System.out.println("\nCadastrando um veículo passeio");
+	public static void cadastrarVeiculoPasseio(Passeio[] vetorPasseio) {
+
+		if (vetorPasseio[4] != null) {
+			System.out.println(
+					"O vetor de de veículos de passeio já está cheio. Não tem mais como cadastrar veículos novos!\n");
+		} else {
+			Scanner input = new Scanner(System.in);
+			Passeio p1 = new Passeio();
+			System.out.println("===================CADASTRO DE VEÍCULO DE PASSEIO=====================\n"
+					+ "Insira os dados e valores solicitados abaixo\n"
+					+ "======================================================================");
+			System.out.print("Placa: ");
+			String placaTentativa = input.nextLine();
+			for (int index = 0; vetorPasseio.length > index; index++) {
+				if (vetorPasseio[index] != null) {
+					if (vetorPasseio[index].getPlaca().equalsIgnoreCase(placaTentativa)) {
+						System.out.println("==================ATENÇÃO====================\n"
+								+ "Já existe um veículo de passeio com a placa: " + placaTentativa + ", informe outra placa!"
+								+ "\n=============================================\n");
+						break;
+					}
+				} else {
+					p1.setPlaca(placaTentativa);
+					System.out.print("Marca: ");
+					p1.setMarca(input.nextLine());
+					System.out.print("Modelo: ");
+					p1.setModelo(input.nextLine());
+					System.out.print("Cor: ");
+					p1.setCor(input.nextLine());
+					System.out.print("Quantidade de Rodas: ");
+					p1.setQdtRodas(input.nextInt());
+					System.out.print("Capacidade de Passageiros: ");
+					p1.setQtdPassageiros(input.nextInt());
+					System.out.print("Velocidade Máxima: ");
+					p1.setVelocMax(input.nextFloat());
+					System.out.print("Potência de Motor: ");
+					p1.getMotor().setPotencia(input.nextInt());
+					System.out.print("Quantidade de Pistões do Motor: ");
+					p1.getMotor().setQtdPist(input.nextInt());
+					// System.out.println(p1);
+
+					vetorPasseio[index] = p1;
+					System.out.println("Veículo de passeio cadastrado com sucesso!\n");
+					break;
+
+				}
+			}
+
+			System.out.print("Deseja cadastrar mais um veículo de passeio (S / N)? ");
+			char cadastrar = 'N';
+			cadastrar = input.next().charAt(0);
+			if (cadastrar == 'S' || cadastrar == 's') {
+				cadastrarVeiculoPasseio(vetorPasseio);
+			}
+
+		}
+
 	}
 
 	public static void cadastrarVeiculoCarga() {
 		System.out.println("\nCadastrando um Veiculo de Carga");
 	}
 
-	public static void imprimirVeiculosPasseio() {
+	public static void imprimirVeiculosPasseio(Passeio[] vetorPasseio) {
 		System.out.println("\nImprimindo todos os veículos de Passeio");
+		int i = 0;
+		boolean empty = true;
+		for (i = 0; vetorPasseio.length > i; i++) {
+			if (vetorPasseio[i] != null) {
+				System.out.println("\nVeículo de indíce: " + (i + 1) + vetorPasseio[i]);
+				empty = false;
+			}
+		}
+		if (empty == true) {
+			System.out.println("Não existem veículos de passeio cadastrados!\n");
+		}
+
 	}
 
 	public static void imprimirVeiculosCarga() {
