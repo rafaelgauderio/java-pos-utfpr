51b78d6a5c5b1d977426cc1d97354fa7eedbd074
Rafael de Luca <deluca1712@gmail.com>
2024-06-16 18:45:39 -0300

    remove object by index and by object

--- src/exemplo/collection/TestCollection.java
+++ src/exemplo/collection/TestCollection.java
@@ -41,6 +41,32 @@ public class TestCollection {
 		for (Person nickname : personDataBase) {
 			System.out.println("Id: " + nickname.getId() + " - Name: " + nickname.getName());
 		}
+
+		// equal compara ojetos, == compara tipos primitivos
+		System.out.println("Removing informing the Id");
+		int removeId = (Integer.parseInt(read.enterData("Inform person id do you want to remove:")));
+
+		for (int i = 0; i < personDataBase.size(); i++) {
+			if (removeId == personDataBase.get(i).getId()) {
+				personDataBase.remove(personDataBase.get(i));
+			} else if (removeId != personDataBase.get(i).getId()) {
+				System.out.println("There is no object with this id on the database!");
+			}
+		}
+
+		System.out.println("\nPrinting object arrayList after exclusion");
+		for (Person nickname : personDataBase) {
+			System.out.println("Id: " + nickname.getId() + " - Name: " + nickname.getName());
+		}
+
+		System.out.println("Removin informing the index");
+		int index = Integer.parseInt(read.enterData("Inform the index of the object to remove:"));
+		personDataBase.remove(index);		
+
+		System.out.println("\nPrinting object arrayList after exclusion");
+		for (Person nickname : personDataBase) {
+			System.out.println("Id: " + nickname.getId() + " - Name: " + nickname.getName());
+		}
 	}
 
 }
