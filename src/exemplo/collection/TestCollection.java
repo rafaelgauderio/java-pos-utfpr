042133b1dfd7edbd3204513e484f130a109d94ff
Rafael de Luca <deluca1712@gmail.com>
2024-06-16 17:55:50 -0300

    printing collection ArrayList

--- src/exemplo/collection/TestCollection.java
+++ src/exemplo/collection/TestCollection.java
@@ -7,27 +7,39 @@ public class TestCollection {
 
 	private static Person person;
 	private static Read read = new Read();
-	private static List<Person> PersonDataBase = new ArrayList<Person>();
+	private static List<Person> personDataBase = new ArrayList<Person>();
 
 	public static void main(String[] args) {
 
 		boolean keepLoop = true;
 
 		while (keepLoop) {
-			
+
 			person = new Person();
 
 			person.setId(Integer.parseInt(read.enterData("Id...: ")));
 			person.setName(read.enterData("Name..: "));
 
-			PersonDataBase.add(person);
+			personDataBase.add(person);
 
-			System.out.println("\nList size: " + PersonDataBase.size());
-			
+			System.out.println("List size: " + personDataBase.size());
+
 			String response = read.enterData("\nAdd more person? (y/n)");
-			if(response.equalsIgnoreCase("n")) {
-				keepLoop=false;				
+			if (response.equalsIgnoreCase("n")) {
+				keepLoop = false;
 			}
+		}
+		System.out.println("Default for");
+		// priting data
+		for (int i = 0; i < personDataBase.size(); i++) {
+			System.out.println("index: " + i + " - Id: " + personDataBase.get(i).getId() + " - Name: "
+					+ personDataBase.get(i).getName());
+
+		}
+
+		System.out.println("ForEach");
+		for (Person nickname : personDataBase) {
+			System.out.println("Id: " + nickname.getId() + " - Name: " + nickname.getName());
 		}
 	}
 
