efa1432dba1d6bfddf3581b644bd5ff1b5d37622
Rafael de Luca <deluca1712@gmail.com>
2024-08-27 11:41:27 -0300

    generics theory

---
+++ src/exemplo/generics/Program.java
@@ -0,0 +1,16 @@
+package exemplo.generics;
+
+public class Program {
+	
+	public static void main (String [] args) {
+		
+		System.out.println("GENERICS\n"
+				+ "\tTermo genérico refere-se a tipos parametrizados.\n"
+				+ "\tPermitem criar classes onde o tipo de dado usado é especificado como parâmetro.\n"
+				+ "\tA vantagem do código genérico é que ele funcionamento aumtomaticamente com qualquer tipo de dado passado.\n"
+				+ "\tEvita ficar fazendo casting a todo momento.\n"
+				+ "\tPropriciam segurança, porque tornam as coerções automática e implícitas.");
+		
+	}
+
+}
